6725ff97f1368327c89f80fa5a1f094853e5af80
Ram Pillai <57178433+RHP043@users.noreply.github.com>
2025-03-30 12:28:51 -0500

    glorilla

--- Assets/ConversationStarter.cs
+++ Assets/ConversationStarter.cs
@@ -2,16 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DialogueEditor;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class ConversationStarter : MonoBehaviour
 {
     [SerializeField] private NPCConversation myConversation;
+    public XRRayInteractor interactor;
+    private bool isInteractable;
+    private InputData InputData;
+    private bool conversationStarted = false;
 
-    private void OnTriggerStay(Collider other)
+    void Start()
     {
-        if (other.CompareTag("Player"))
+        if (interactor != null)
+        {
+            interactor.hoverEntered.AddListener(OnHoverEnter);
+            interactor.hoverExited.AddListener(OnHoverExit);
+        }
+        else
+        {
+            Debug.LogError("XRRayInteractor is not assigned to ConversationStarter.");
+        }
+
+        GameObject myXROrigin = GameObject.Find("XR Origin");
+        if (myXROrigin != null)
+        {
+            InputData = myXROrigin.GetComponent<InputData>();
+        }
+    }
+
+    private void OnHoverEnter(HoverEnterEventArgs args)
+    {
+        isInteractable = true;
+    }
+
+    private void OnHoverExit(HoverExitEventArgs args)
+    {
+        isInteractable = false;
+        conversationStarted = false; // Reset when leaving
+    }
+
+    void Update()
+    {
+        if (InputData == null || InputData.RController == null) return;
+
+        bool triggerPressed = false;
+        InputData.RController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerPressed);
+
+        if (isInteractable && triggerPressed && !conversationStarted)
         {
             ConversationManager.Instance.StartConversation(myConversation);
+            conversationStarted = true; // Prevents immediate retriggering
         }
     }
 }
